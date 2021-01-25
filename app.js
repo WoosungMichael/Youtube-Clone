f9c445fd1eb38071ad9e7318002e02d3b661556a
WoosungMichael <woosung0420k@naver.com>
2021-01-25 16:50:31 +0900

    using MVC

--- app.js
+++ app.js
@@ -4,18 +4,24 @@ import morgan from "morgan"; //앞의 morgan 자리에 별명으로 써도 됨
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import {userRouter} from "./router"; //default로 export한게 아닌 경우
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter"; //url을 분리하는 과정
+import routes from "./routes";
+
+//import {userRouter} from "./routers/userRouter"; //default로 export한게 아닌 경우
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from home");
 /*
+const handleHome = (req, res) => res.send("Hello from home");
+==same as
 function handleHome(req, res){
     res.send("Hello from home");
 }
-*/
+
 
 const handleProfile = (req, res) => res.send("You are on my profile");
-/*
+==same as
 function handleProfile(req, res){
     res.send("You are on my profile");
 }
@@ -36,10 +42,13 @@ app.use(morgan("dev"));
 //app.use(betweenHome); //순서 중요 get전에 쓴 모든 middleware 실행
 //middleware에서 res.send를 실행하면 연결을 끊을 수 있다.
 
+/*
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile);
+*/
 
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app; //누군가 내 파일을 import할 때 app object를 주겠다는 뜻 
---
+++ controllers/userController.js
@@ -0,0 +1,14 @@
+export const join = (req, res) => res.send("Join");
+export const login = (req, res) => res.send("Login");
+export const logout = (req, res) => res.send("Logout");
+export const users = (req, res) => res.send("Users");
+export const userDetail = (req, res) => res.send("User Detail");
+export const editProfile = (req, res) => res.send("Edit Profile");
+export const changePassword = (req, res) => res.send("Change Password");
+
+//=> : implicit return
+/*
+    lalla = () => {
+        return true
+    }  : 중괄호를 적어주면 암시적 성격을 잃어서 return을 적어줘야함
+*/
