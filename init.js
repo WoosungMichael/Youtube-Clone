9c1f752bff4c59ed14bf2dfe3f402997c04ac890
WoosungMichael <woosung0420k@naver.com>
2021-01-31 02:28:43 +0900

    Video Model

--- init.js
+++ init.js
@@ -1,8 +1,12 @@
+import "./db";
 import app from "./app"; //default로 export하는 경우
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-    console.log(`✅ Listening on: http://localhost:${PORT}`);
+console.log(`✅  Listening on: http://localhost:${PORT}`);    
 
 app.listen(PORT, handleListening);
---
+++ models/Video.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+  fileUrl: {
+    type: String,
+    required: "File URL is required"
+  },
+  title: {
+    type: String,
+    required: "Tilte is required"
+  },
+  description: String,
+  views: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
+});
+
+//definition(schema)를 이용하여 실제 document를 만드는 것.
+const model = mongoose.model("Video", VideoSchema); //model의 이름 "Video", 이 모델의 schema는 VideoSchema
+export default model;
