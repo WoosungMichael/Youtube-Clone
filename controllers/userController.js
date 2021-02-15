3bd41c252ca9843db701b527e72c85dda403bace
WoosungMichael <woosung0420k@naver.com>
2021-02-15 23:45:06 +0900

    User Profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -116,7 +116,7 @@ export const userDetail = async (req, res) => {
   }
 };
 
-export const editProfile = (req, res) => res.render("editProfile");
+export const getEditProfile = (req, res) => res.render("editProfile");
 export const changePassword = (req, res) => res.render("changePassword");
 
 //=> : implicit return
